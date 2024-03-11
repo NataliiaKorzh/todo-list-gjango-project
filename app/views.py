f512c1810e70dd50acf6234c7599f9622b0235f5
Nataliia Korzh <clarioruberia@gmail.com>
2024-03-11 14:59:53 +0200

    create views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from app.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("app:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("app:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("app:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("app:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("app:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("app:tag-list")
+
+
+def toggle_status_of_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.status = not task.status
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("app:task-list"))
